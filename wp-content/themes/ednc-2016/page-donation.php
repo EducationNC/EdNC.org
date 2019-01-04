c77d860bd23bda4d30034633bcee7f89954609fc
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-01-04 13:40:32 -0500

    removed extra code

--- wp-content/themes/ednc-2016/page-donation.php
+++ wp-content/themes/ednc-2016/page-donation.php
@@ -47,13 +47,4 @@
       </div>
     </div>
   </div>
-
-
-
-    <!-- <script src=”https://donorbox.org/widget.js” paypalExpress=”false”></script>
-    <iframe src=”https://donorbox.org/embed/donation-page-8″
-    height=”685px” width=”100%” style=”max-width:500px; min-width:310px; max-height:none!important”
-    seamless=”seamless” name=”donorbox” frameborder=”0″ scrolling=”no” allowpaymentrequest>
-    </iframe> -->
-
 <?php endwhile; ?>

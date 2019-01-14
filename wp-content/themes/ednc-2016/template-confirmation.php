5aaa60c1baf0d8c196ca33bc5d918c6426a323f1
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-01-14 15:09:42 -0500

    metadata

---
+++ wp-content/themes/ednc-2016/template-confirmation.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template Name: Confirmation
+ * Template Post Type: page, product
+ */
+?>
+<div class="container">
+  <div class="row donate">
+    <div class="col-md-12">
+      <div class="donate-content">
+        <h3>Thank you! We will be in touch with you soon.</h3>
+      </div>
+    </div>
+  </div>
+</div>
